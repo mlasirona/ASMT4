48e8d084b2dab1f4f19d86abe38e1d51a0bb541b
Tung Nguyen <tung.nguyen23797@gmail.com>
2021-11-15 11:50:17 -0800

    modify code

--- asmt_files/LinkedBag340.cpp
+++ asmt_files/LinkedBag340.cpp
@@ -12,128 +12,87 @@
 
 
 template<typename ItemType>
-bool LinkedBag<ItemType>::removeSecondNode340() {
-    bool canRemoveItem = !isEmpty() && (headPtr->getNext() != nullptr);
+bool LinkedBag<ItemType>::removeSecondNode340(){ // done(?)
+    if(headPtr != nullptr || headPtr->next != nullptr){
+        Node<ItemType>* second = headPtr->next->next;
 
-    if (canRemoveItem) {
-        Node<ItemType>* nodeToDeletePtr = headPtr->getNext();
-        headPtr->setNext(nodeToDeletePtr->getNext());
+        remove(second);
 
-        nodeToDeletePtr->setNext(nullptr);
-        delete nodeToDeletePtr;
-        nodeToDeletePtr = nullptr;
-
-        itemCount--;
+        return true;
     }
-
-    return canRemoveItem;
+    return false;
 }
-template<typename ItemType>
-bool LinkedBag<ItemType>::addEnd340(const ItemType& newEntry) {
 
-    Node<ItemType>* newNodePtr = new Node<ItemType>();
-    newNodePtr->setItem(newEntry);
-    newNodePtr->setNext(nullptr);
+template<typename ItemType>
+bool LinkedBag<ItemType>::addEnd340(const ItemType& newEntry){ // done(?)
     Node<ItemType>* curPtr = headPtr;
 
-    while (curPtr->getNext() != nullptr) {
+    Node<ItemType>* temp = new Node<ItemType>();
+    temp->setItem(newEntry);
+
+    while(curPtr->next != nullptr){
         curPtr = curPtr->getNext();
     }
-    //at this point the curPtr is the last pointer
-    curPtr->setNext(newNodePtr);
-    itemCount++;
+
+    curPtr->next->setNext(temp);
+    temp->setNext(nullptr);
+
     return true;
 }
 
 template<typename ItemType>
-int LinkedBag<ItemType>::getCurrentSize340Iterative() const {
+int LinkedBag<ItemType>::getCurrentSize340Iterative() const{ // done(?)
     Node<ItemType>* curPtr = headPtr;
-    int size = 1;
-    while ((curPtr->getNext() != nullptr)) {
-        curPtr = curPtr->getNext();
-        size++;
-    }
-    return size;
-}
-template<typename ItemType>
-int LinkedBag<ItemType>::getCurrentSize340RecursiveHelper(Node<ItemType>* node) const {
-    if (node == nullptr)
-        return 0;
-    node = node->getNext();
-    return 1 + getCurrentSize340RecursiveHelper(node);
+    int counter = 0;
 
-}
-template<typename ItemType>
-int LinkedBag<ItemType>::getCurrentSize340Recursive() const {
-
-    return getCurrentSize340RecursiveHelper(headPtr);
-}
-
-template<typename ItemType>
-int LinkedBag<ItemType>::getCurrentSize340RecursiveNoHelper() const {
-    if (headPtr == nullptr)
-        return 0;
-
-    Node<ItemType>* nextNode = this->headPtr->getNext();
-    if(nextNode!= nullptr){
-        LinkedBag<ItemType> links;
-        links.add(nextNode->getItem());
-        return 1+ links.getCurrentSize340RecursiveNoHelper();
-    }
-    return 1;
-}
-template<typename ItemType>
-int LinkedBag<ItemType>::getFrequencyOf340RecursiveHelper(Node<ItemType>* node, const ItemType& anEntry) const {
-    if (node == nullptr)
-        return 0;
-
-    int frequency = 0;
-    if (anEntry == node->getItem()) {
-        frequency = 1;
-    }
-    node = node->getNext();
-    return frequency + getFrequencyOf340RecursiveHelper(node, anEntry);
-}
-
-template<typename ItemType>
-int LinkedBag<ItemType>::getFrequencyOf340Recursive(const ItemType& anEntry) const {
-    return getFrequencyOf340RecursiveHelper(headPtr, anEntry);
-}
-template<typename ItemType>
-int LinkedBag<ItemType>::getFrequencyOf340RecursiveNoHelper(const ItemType& anEntry) const {
-    if (headPtr == nullptr)
-        return 0;
-
-    Node<ItemType>* nextNode = this->headPtr->getNext();
-    if(nextNode!= nullptr){
-
-        LinkedBag<ItemType> links;
-        links.add(nextNode->getItem());
-        if (anEntry == nextNode->getItem()) {
-            return 1 + links.getFrequencyOf340RecursiveNoHelper(anEntry);
-        } else
-            return 0 + links.getFrequencyOf340RecursiveNoHelper(anEntry);
-    }
-
-    if (anEntry == headPtr->getItem()) {
-        return 1;
-    } else
-        return 0 ;
-}
-template<typename ItemType>
-ItemType LinkedBag<ItemType>::removeRandom340() {
-    int max = itemCount, min = 1, counter = 1;
-    int rNum = rand() % (max - min + 1) + min;
-    Node<ItemType>* curPtr = headPtr;
-    while (curPtr->getNext() != nullptr) {
-        if (counter == rNum) {
-            ItemType itemToRemove = curPtr->getItem();
-            remove(itemToRemove);
-            return itemToRemove;
-        }
+    while(curPtr != nullptr){
         counter++;
         curPtr = curPtr->getNext();
     }
-    return {};
+
+    return counter;
 }
 
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340Recursive() const{ // look at indirect recursion for help
+
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340RecursiveNoHelper() const{ // need a global variable for NoHelpers (?)
+/*     int sum;
+    Node<ItemType>* curPtr = headPtr;
+    if (curPtr == nullptr){
+        sum = 1;
+    }
+    else {
+    }
+    return sum;
+*/
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340Recursive(const ItemType&) const{ // look at indirect recursion for help
+
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340RecursiveNoHelper(const ItemType&) const{
+
+}
+
+template<typename ItemType>
+ItemType LinkedBag<ItemType>::removeRandom340(){
+
+}
+
+// helper functions
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340RecursiveHelper(Node<ItemType>*) const{ // look at indirect recursion for help
+
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340RecursiveHelper(Node<ItemType>*, const ItemType&) const{ // look at indirect recursion for help
+
+} 
